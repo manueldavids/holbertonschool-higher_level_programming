477d16e74d60fec3d59caac3560f793f1b0a7a7c
voltamonkey <9461@holbertonstudents.com>
2025-01-17 19:51:14 -0400

    Fix script to print numbers from 0 to 99 with correct formatting

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-# Print numbers from 0 to 99 in ascending order, separated by ", "
+# Print numbers from 0 to 99 in ascending order with proper formatting
 for number in range(100):  # Loop from 0 to 99
-    if number != 99:  # For numbers 0 to 98, include ", " after the number
-        print("{:02d}, ".format(number), end="")
-    else:  # For the last number (99), print without ", " and with a newline
-        print("{:02d}".format(number))
+    print("{:02d}".format(number), end=", " if number != 99 else "\n")

323e909fbf18c520ed08ca507f3295c80997aa0f
voltamonkey <9461@holbertonstudents.com>
2025-01-17 19:37:26 -0400

    Fix output issue for printing lowercase ASCII alphabet in one line

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 # Print the ASCII alphabet in lowercase without a newline after each character
-print("".join("{:c}".format(letter) for letter in range(97, 123)))
+for letter in range(97, 123):
+    print("{:c}".format(letter), end="")

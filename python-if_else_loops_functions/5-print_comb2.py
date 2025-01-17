6067bbd66bdf8f0412303219f42e6d51f8d3995e
voltamonkey <9461@holbertonstudents.com>
2025-01-17 19:45:34 -0400

    Add script to print numbers from 0 to 99 in ascending order with formatting

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# Print numbers from 0 to 99 in ascending order, separated by ", "
+for number in range(100):  # Loop from 0 to 99
+    if number < 99:  # For numbers from 0 to 98
+        print("{:02d}".format(number), end=", ")
+    else:  # For the last number (99), print with a newline
+        print("{:02d}".format(number))

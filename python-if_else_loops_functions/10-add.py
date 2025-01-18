4519d3135245b42d14cc34c9001a61035f0d04d6
voltamonkey <9461@holbertonstudents.com>
2025-01-17 20:08:15 -0400

    Add function to calculate the sum of two integers

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """
+    Add two integers and return the result.
+
+    Args:
+        a: The first integer.
+        b: The second integer.
+
+    Returns:
+        The sum of a and b.
+    """
+    return a + b

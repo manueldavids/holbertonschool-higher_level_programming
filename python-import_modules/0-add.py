5b83261f4796bf47f2d148386adcb5a9dbd09097
voltamonkey <9461@holbertonstudents.com>
2025-01-17 22:38:09 -0400

    Add example scripts demonstrating the use of an imported add function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """
-    Import the function add(a, b) from add_0.py and use it to print
-    the result of 1 + 2 = 3.
-    """
-    from add_0 import add  # Import the add function only once
+    from add_0 import add
 
-    # Define variables a and b in separate lines
     a = 1
     b = 2
-
-    # Print the result using the add function
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))

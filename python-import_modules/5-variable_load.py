9e81cf53c6b4f9ea358ed964768be3b07c6d3eaa
voltamonkey <9461@holbertonstudents.com>
2025-01-17 23:14:49 -0400

    Add 5-variable_load.py to import and print variable a from variable_load_5.py

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    # Import the variable `a` from the file `variable_load_5.py`
+    from variable_load_5 import a
+
+    # Print the value of `a`
+    print(a)

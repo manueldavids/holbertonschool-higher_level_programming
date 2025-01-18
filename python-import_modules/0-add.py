7c8d42623520682effa5eba343047ee0a39685e0
voltamonkey <9461@holbertonstudents.com>
2025-01-17 20:38:22 -0400

    Add script to perform addition using an imported function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,11 +5,12 @@ if __name__ == "__main__":
     Import the function add(a, b) from add_0.py and use it to print
     the result of 1 + 2 = 3.
     """
-    from add_0 import add  # Import the add function only once
+    from add_0 import add  # Import add function only once
 
-    a = 1  # Define variable a
-    b = 2  # Define variable b
+    # Define variables a and b in separate lines
+    a = 1
+    b = 2
 
-    # Call the add function and print the result in the required format
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    # Call the add function and print the result
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 

3e99f43a47487e06a64280643fbfbf4a859ded2c
voltamonkey <9461@holbertonstudents.com>
2025-01-17 22:42:44 -0400

    Uploading

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from add_0 import add
+    """
+    Import the function add(a, b) from add_0.py and use it to print
+    the result of 1 + 2 = 3.
+    """
+    from add_0 import add  # Import the add function only once
 
+    # Define variables a and b in separate lines
     a = 1
     b = 2
+
+    # Print the result using the add function
     print("{} + {} = {}".format(a, b, add(a, b)))

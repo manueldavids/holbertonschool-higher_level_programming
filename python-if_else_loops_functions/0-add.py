0a85e8771bfec42f2df534eba1b035371555c742
voltamonkey <9461@holbertonstudents.com>
2025-01-17 20:19:47 -0400

    Add script to use imported add function for addition

---
+++ python-if_else_loops_functions/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """
+    Import the function add(a, b) from add_0.py and use it to print
+    the result of 1 + 2 = 3.
+    """
+    from add_0 import add
+
+    a = 1  # Define variable a
+    b = 2  # Define variable b
+
+    # Call the add function and print the result
+    print("{} + {} = {}".format(a, b, add(a, b)))

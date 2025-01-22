e78162e1dfc724ce81e9372d76f63b4de53be0a4
voltamonkey <9461@holbertonstudents.com>
2025-01-21 22:10:25 -0400

    Add function to find common elements in two sets

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    Find common elements in two sets.
+
+    Args:
+        set_1: The first set.
+        set_2: The second set.
+
+    Returns:
+        A set containing the common elements of set_1 and set_2.
+    """
+    return set_1 & set_2

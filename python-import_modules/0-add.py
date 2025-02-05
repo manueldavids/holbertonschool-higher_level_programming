5641ffe669c64ba19f7c33c0ba50092b72d2a7bd
voltamonkey <9461@holbertonstudents.com>
2025-02-05 07:12:18 -0400

    Add add_0.py and 0-add.py for addition functionality

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
+from add_0 import add
 
-if __name__ == "__main__":
-    """
-    Import the function add(a, b) from add_0.py and use it to print
-    the result of 1 + 2 = 3.
-    """
-    from add_0 import add  # Import the add function only once
-
-    # Define variables a and b in separate lines
-    a = 1
-    b = 2
-
-    # Print the result using the add function
-    print("{} + {} = {}".format(a, b, add(a, b)))
+a = 1
+b = 2
+result = add(a, b)
+print(f"{a} + {b} = {result}")

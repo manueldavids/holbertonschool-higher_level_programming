eb8d99b7e36fc4b0ee097887094deee38fa1cdf8
voltamonkey <9461@holbertonstudents.com>
2025-02-07 14:57:49 -0400

    "Implement safe_print_list function with exception handling"

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    y = 0
-    for i in range(x):
-        try:
-            print('{:d}'.format(my_list[i]), end="")
-            y += 1
-        except IndexError:
-            break
-    print()
-    return y
+    count = 0  # Counter for printed elements
+    try:
+        for i in range(x):  # Attempt to print x elements
+            print(my_list[i], end="")  # Print without newline
+            count += 1  # Increase count for each printed element
+    except IndexError:  # Handle case where x is larger than the list
+        pass  # Do nothing, just exit loop
+
+    print()  # Print a new line after printing elements
+    return count  # Return the number of printed elements
+

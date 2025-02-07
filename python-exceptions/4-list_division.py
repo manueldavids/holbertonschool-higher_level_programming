2d1a836fd5f1369966fd3cd3773f5914662a27a2
voltamonkey <9461@holbertonstudents.com>
2025-02-07 15:16:25 -0400

    "Fix PEP 8 E501 error by breaking long lines and improving exception handling"

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -12,21 +12,21 @@ def list_division(my_list_1, my_list_2, list_length):
         A new list with the results of the divisions.
     """
     new_list = []
+
     for i in range(list_length):
         try:
-            a = my_list_1[i]
-            b = my_list_2[i]
-            if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+            num1 = my_list_1[i]
+            num2 = my_list_2[i]
+            result = num1 / num2
+        except (TypeError, ZeroDivisionError, IndexError) as e:
+            if isinstance(e, TypeError):
                 print("wrong type")
-                result = 0
+            elif isinstance(e, ZeroDivisionError):
+                print("division by 0")
             else:
-                result = a / b
-        except ZeroDivisionError:
-            print("division by 0")
-            result = 0
-        except IndexError:
-            print("out of range")
+                print("out of range")
             result = 0
         finally:
             new_list.append(result)
+
     return new_list

bf37eb983f140ce0ba3d72557df8ef7151dbae0e
voltamonkey <9461@holbertonstudents.com>
2025-02-11 02:22:09 -0400

    +

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
+from add_0 import add  # Importa solo la función add
 
-a = 1
-b = 2
-result = add(a, b)
-print(f"{a} + {b} = {result}")
+a = 1  # Definimos la variable a en una línea
+b = 2  # Definimos la variable b en otra línea
+
+print("{} + {} = {}".format(a, b, add(a, b)))  # Imprimimos el resultado usando string format
+

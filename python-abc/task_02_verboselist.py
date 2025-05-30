dd184702472b75fe58e580ed3cdda4d6dc661c3e
manueldavids <manuelsantanadev@proton.me>
2025-05-30 17:13:00 -0400

    Add VerboseList class with notifications for append, extend, remove, and pop (PEP8)

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module that defines VerboseList class with notifications on list operations."""
+
+
+class VerboseList(list):
+    """List subclass that prints notifications on modifications."""
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, iterable):
+        count = len(iterable)
+        super().extend(iterable)
+        print(f"Extended the list with [{count}] items.")
+
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = self[index]
+        print(f"Popped [{item}] from the list.")
+        return super().pop(index)

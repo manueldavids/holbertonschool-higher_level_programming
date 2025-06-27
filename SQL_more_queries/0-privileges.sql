ada20a2756658a2c3c8a123c5102dbd21380b305
manueldavids <manuelsantanadev@proton.me>
2025-06-27 16:18:34 -0400

    Add MySQL user privileges script

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- Script to list privileges for user_0d_1 and user_0d_2
+-- This script shows all privileges for the specified users
+
+-- Show privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

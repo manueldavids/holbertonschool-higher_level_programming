dc7866c33381105e28e6e4d64052265c31ce7f9e
manueldavids <manuelsantanadev@proton.me>
2025-07-27 19:40:03 -0400

    feat: Add script that prints three programming languages using multiple console.log statements

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+/**
+ * Task 1: 3 languages
+ *
+ * This script demonstrates:
+ * - How to use multiple console.log() statements
+ * - Basic string output in JavaScript
+ * - Sequential execution of statements
+ *
+ * Requirements:
+ * - Print "C is fun" on first line
+ * - Print "Python is cool" on second line
+ * - Print "JavaScript is amazing" on third line
+ * - Don't use 'var' keyword
+ */
+
+// Print the first line about C programming
+console.log('C is fun');
+
+// Print the second line about Python programming
+console.log('Python is cool');
+
+// Print the third line about JavaScript programming
+console.log('JavaScript is amazing');

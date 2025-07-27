76d77a3f24a7cfa383531cb027a5f8a0ddb20793
manueldavids <manuelsantanadev@proton.me>
2025-07-27 18:12:10 -0400

    feat: Add first JavaScript script that prints "JavaScript is amazing" using const variable

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+/**
+ * Task 0: First constant, first print
+ *
+ * This script demonstrates:
+ * - How to create a constant variable using 'const'
+ * - How to use console.log() for output
+ * - Basic JavaScript syntax and structure
+ *
+ * Requirements:
+ * - Create a constant variable called 'myVar'
+ * - Print "JavaScript is amazing"
+ * - Don't use 'var' keyword
+ */
+
+// Create a constant variable with the required message
+const myVar = 'JavaScript is amazing';
+
+// Print the message using console.log()
+console.log(myVar);

4efa570c17ca8b9abfab42a1aaaf0c6dac0bdcb3
manueldavids <manuelsantanadev@proton.me>
2025-07-27 19:42:39 -0400

    feat: Add script that prints different messages based on command line argument count

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+/**
+ * Task 2: Arguments
+ *
+ * This script demonstrates:
+ * - How to access command line arguments using process.argv
+ * - How to use conditional statements (if/else)
+ * - How to count array elements
+ * - Basic logic flow control
+ *
+ * Requirements:
+ * - Print "No argument" if no arguments passed
+ * - Print "Argument found" if one argument passed
+ * - Print "Arguments found" if multiple arguments passed
+ * - Don't use 'var' keyword
+ * - Use process.argv to access arguments
+ */
+
+// Get the number of actual arguments (excluding node and script path)
+const argumentCount = process.argv.length - 2;
+
+// Check the number of arguments and print appropriate message
+if (argumentCount === 0) {
+  console.log('No argument');
+} else if (argumentCount === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+} 

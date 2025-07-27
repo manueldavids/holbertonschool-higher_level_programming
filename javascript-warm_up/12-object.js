af7926edad7227c2722c9de48160acdc2fa7c194
manueldavids <manuelsantanadev@proton.me>
2025-07-27 19:58:18 -0400

    feat: Add script that demonstrates object property modification from 12 to 89

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+/**
+ * Task 12: Object
+ *
+ * This script demonstrates:
+ * - How to work with objects in JavaScript
+ * - How to access and modify object properties
+ * - How to log objects to console
+ * - Basic object manipulation
+ *
+ * Requirements:
+ * - Update the script to replace value 12 with 89
+ * - Don't use 'var' keyword
+ */
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+// Update the value property from 12 to 89
+myObject.value = 89;
+console.log(myObject);

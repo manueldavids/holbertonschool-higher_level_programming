1ea9d7fdc567a450467724d927f44baae1d463fb
manueldavids <manuelsantanadev@proton.me>
2025-07-27 20:17:27 -0400

    feat: implement color change script using document.querySelector

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,11 @@
+// Task 0: Color Me
+// This script updates the text color of the header element to red (#FF0000)
+// using document.querySelector to select the HTML tag
+
+// Select the header element using document.querySelector
+// This method returns the first element that matches the CSS selector
+const header = document.querySelector('header');
+
+// Change the text color to red (#FF0000)
+// We use the style property to modify the element's CSS styles
+header.style.color = '#FF0000'; 
